91d1504b4df800b829798049a1144035c17bd3ff
j-bitanga <jbitanga88@gmail.com>
2021-03-19 16:50:29 -0700

    index changed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -27,9 +28,9 @@ let notes = [
     }
   ]
 
-  app.get('/', (request, response) => {
-      response.send('<h1> Hello World~!</h1>')
-  })
+  /*app.get('/', (request, response) => {
+      response.send('<h1> Hello World!</h1>')
+  })*/
 
   app.get('/api/notes', (request, response) => {
       response.json(notes)
