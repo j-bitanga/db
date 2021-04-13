80758c1932814fbfbe6b5ba6c3f91e792395ccff
j-bitanga <jbitanga88@gmail.com>
2021-04-13 04:53:48 -0700

    'mongo js changes'

--- mongo.js
+++ mongo.js
@@ -25,8 +25,15 @@ const note = new Note({
   important: true,
 })
 
-note.save().then(result => {
+/*note.save().then(result => {
   console.log('note saved!')
+  mongoose.connection.close()
+})*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
 
